3891d1a972efa21ec560e4ce82fec709db944e33
Minji Park <53109557+min0731@users.noreply.github.com>
2023-12-07 15:39:23 +0900

    [level 2] Title: 의상, Time: 0.26 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,27 @@
 import java.util.*;
 
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer =1;
-        HashMap <String, Integer> map = new HashMap<>();
+        int answer = 1;
         
-        //getOrDefalut를 사용 -> 있으면 +1, 없으면 1해줌 
-        for (String [] cloth : clothes){
-            map.put(cloth[1], map.getOrDefault(cloth[1],0)+1);
-        }
+        HashMap <String, Integer> map = new HashMap <>();
         
-        // 조합일때는 어떻게 구현해야하지?
-         int result = 1;
-        for(String key : map.keySet()){
-            result *= (map.get(key) + 1);
+        for (int i=0; i<clothes.length; i++){
+            // 카테고리에 따라 카운트
+            // 없으면 0에서 +1 , 있으면 기존 카테고리 갯수에 +1 
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1);
         }
-        return result-1;
+            
+        // 의상별 조합 
+        // 상의: 0/1/2개   하의: 0/1/2/3개 
+        // 그러나 뭐든 하나는 걸쳐야하니 0,0일때만 빼줌 (-1)
+        
+        // 모든 키들을 반환
+        for (String key: map.keySet()){
+            answer *= map.get(key)+1;
+        }
+        System.out.println(map.entrySet());
+    return answer -1;
     }
 }
