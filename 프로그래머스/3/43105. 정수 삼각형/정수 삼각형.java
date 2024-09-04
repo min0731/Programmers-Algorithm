e000d9c442ec5a77c53c9f19727325c0f9c2f317
min0731 <53109557+min0731@users.noreply.github.com>
2024-09-04 14:34:47 +0900

    [level 3] Title: 정수 삼각형, Time: 9.95 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,30 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = -1;
-        int dp [][]= new int[triangle.length][triangle.length];
-        
-        // 첫 번째 숫자는 바로 넣어줌
+        int answer = 0;
+        int dp[][] = new int[triangle.length][triangle.length];
+
+        // 첫 번째 숫자는 바로 초기화
         dp[0][0] = triangle[0][0];
-        
-        // 삼각형의 각 줄에 대해 DP 테이블을 채워 나감
-        for (int j = 1; j < triangle.length; j++) {
-            // 왼쪽 가장자리의 값 (한 방향에서만 올 수 있음)
-            dp[j][0] = dp[j - 1][0] + triangle[j][0];
-            // 오른쪽 가장자리의 값 (한 방향에서만 올 수 있음)
-            dp[j][j] = dp[j - 1][j - 1] + triangle[j][j];
-            
-            // 중간 값들 처리 (두 방향에서 올 수 있음)
-            for (int i = 1; i < j; i++) {
-                dp[j][i] = Math.max(dp[j - 1][i - 1], dp[j - 1][i]) + triangle[j][i];
+
+        // DP 테이블 채우기
+        for (int i = 1; i < triangle.length; i++) {
+            // 왼쪽 가장자리 값 (위에서 한 경로로만 내려옴)
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+
+            // 오른쪽 가장자리 값 (위에서 한 경로로만 내려옴)
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
+
+            // 중간의 값들 (위에서 두 경로 중 더 큰 값 선택)
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
             }
         }
-        
+
         // 마지막 행에서 가장 큰 값을 찾아서 반환
-        for (int i = 0; i < dp[dp.length - 1].length; i++) {
-            answer = Math.max(dp[dp.length - 1][i], answer);
+        for (int i = 0; i < triangle[triangle.length - 1].length; i++) {
+            answer = Math.max(dp[triangle.length - 1][i], answer);
         }
-        
+
         return answer;
     }
 }
