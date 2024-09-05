e50c4b2349e0528026c432f561c79886dbdf0255
min0731 <53109557+min0731@users.noreply.github.com>
2024-09-05 18:14:44 +0900

    [level 2] Title: 소수 찾기, Time: 22.00 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,52 @@
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
+
 
 class Solution {
-    HashSet<Integer> numbersSet = new HashSet<>();
-
-    public boolean isPrime(int num) {
-        // 1. 0과 1은 소수가 아니다
-        if (num == 0 || num == 1)
-            return false;
-
-        // 2. 에라토스테네스의 체의 limit 숫자를 계산한다.
-        int lim = (int)Math.sqrt(num);
-
-        // 3. 에라토스테네스의 체에 따라 lim까지 배수 여부를 확인한다.
-        for (int i = 2; i <= lim; i++)
-            if (num % i == 0)
-                return false;
-
-        return true;
-    }
-
-    public void recursive(String comb, String others) {
-        // 1. 현재 조합을 set에 추가한다.
-        if (!comb.equals(""))
-            numbersSet.add(Integer.valueOf(comb));
-
-        // 2. 남은 숫자 중 한 개를 더해 새로운 조합을 만든다.
-        for (int i = 0; i < others.length(); i++){
-            recursive(comb + others.charAt(i), others.substring(0, i) + others.substring(i + 1));
-        System.out.println(others +" "+ others.substring(0, i)+" "+  others.substring(i + 1));
-        }
-    }
-
+    static  Set<Integer> set;
+    static boolean check [];
     public int solution(String numbers) {
-        // 1. 모든 조합의 숫자를 만든다.
-        recursive("", numbers);
-
-        // 2. 소수의 개수만 센다.
-        int count = 0;
-        Iterator<Integer> it = numbersSet.iterator();
-        while (it.hasNext()) {
-            int number = it.next();
-            if (isPrime(number))
-                count++;
+        int answer = 0;
+        set = new HashSet<>();
+        String s ="";
+        
+        char[] arr = numbers.toCharArray();
+        check = new boolean [numbers.length()];
+        
+        for (int i=1; i<=numbers.length(); i++){
+                getPrime(s, arr, i, 0);   
         }
-
-        // 3. 소수의 개수를 반환한다.
-        return count;
+          // 소수만 추출하여 answer에 카운트
+        for (int num : set) {
+            if (isPrime(num)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void getPrime ( String s, char[] arr, int N, int cnt){
+        if (N == cnt){ 
+            set.add(Integer.parseInt(s));
+            
+            return;
+        }
+        
+        for (int i=0; i< arr.length; i++){
+            if(!check[i]){
+                check[i]= true;
+                getPrime(s+arr[i], arr,N,cnt+1);
+                check[i]=false;
+            }
+        }
+    }
+    
+       // 소수 판별 함수
+    public boolean isPrime(int num) {
+        if (num < 2) return false;  // 1보다 작은 숫자는 소수가 아님
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
     }
 }
