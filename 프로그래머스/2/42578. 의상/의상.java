6d854a3b838813d753f842ab31a11a0af4cf78eb
min0731 <53109557+min0731@users.noreply.github.com>
2024-09-06 14:55:31 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,25 +3,19 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
         int answer = 1;
         
-        HashMap <String, Integer> map = new HashMap <>();
-        
-        for (int i=0; i<clothes.length; i++){
-            // 카테고리에 따라 카운트
-            // 없으면 0에서 +1 , 있으면 기존 카테고리 갯수에 +1 
-            // 그러나 선탣하지 않아도 된다는 목록이 있으니 그 선택지를 포함한 1부터 시작
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],1)+1);
+        // hash값에 넣어줌 -> get을 통해 가져올 수 없으므로 넣음과 동시에 중복 확인 
+        // key-value의 관계: value의 값은 중복이 됨 -> 
+        for (String [] s : clothes){
+            map.put(s[1], map.getOrDefault(s[1],0)+1);
         }
-            
-        // 의상별 조합 
-        // 상의: 0/1/2개   하의: 0/1/2/3개 
-        // 그러나 뭐든 하나는 걸쳐야하니 0,0일때만 빼줌 (-1)
         
-        // 모든 키들을 반환
         for (String key: map.keySet()){
-            answer *= map.get(key);
+             answer *= map.get(key)+1;
         }
-    return answer -1;
+        
+        return answer-=1;
     }
 }
