58aba2c4e744a646961899e87091fa7bee0b07b0
min0731 <53109557+min0731@users.noreply.github.com>
2024-09-30 13:54:15 +0900

    [level 3] Title: 단속카메라, Time: 11.54 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,21 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        // 첫 진출된 곳에는 CCTV를 설치 
         int answer = 1;
         
-        // 두번째 기준 내림차순으로 정렬
-        Arrays.sort(routes,(a,b) -> Integer.compare(a[1],b[1]));
-        int camera =routes[0][1];  
-        
+        Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
+        int camera = routes[0][1];
         
         for (int i=1; i<routes.length; i++){
-            //해당 범위 안에 안들어오면
-            if(camera < routes[i][0]) {
-                answer+=1;
-                camera = routes[i][1]; 
+            if(routes[i][0] > camera) {
+                camera = routes[i][1];
+                answer+=1;   
             }
-                
         }
         return answer;
     }
