8eedd43b094df93f45d4a9eab8f5e87d634171c8
min0731 <53109557+min0731@users.noreply.github.com>
2024-10-15 14:11:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 9517.77 ms, Memory: 558 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,30 +1,51 @@
 import java.util.*;
+/*
+    숫자의 위치가 변경되면 안되므로 -> Stack을 사용
+*/
 
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
-        Stack<Character> st = new Stack<>();
+        Stack <Character>  st = new Stack<>();
+        int delete=0;
         
-        for (int i=0; i< number.length(); i++){
-            char c= number.charAt(i);
+        char ch[] = number.toCharArray();
+        
+        st.push(ch[0]);
+        for (int i=1; i<ch.length; i++){
             
-            while(!st.isEmpty()&& k>0 && st.peek() < c){
-                     st.pop();
-                     k--;
-                  }
-             
-            st.push(c);
+            // 스택에 내용물이 있다면? , 마지막 종료조건
+            if (delete ==k ) {
+                st.push(ch[i]);
+                continue;
+            }
+            
+            else
+            {  
+                while (st.size()>0 && st.peek()-'0'< ch[i]-'0') {
+                    st.pop();
+                    delete+=1;
+                    
+                    if (delete == k ) break;
+                }
+                st.push(ch[i]);
+            }
         }
-        // k가 제거되지 않는 경우는, 내림차순으로 있을 때
-        while(k >0){
+
+        // 남은 숫자를 제거하지 못한 경우 (76544321 인경우 push만 했기에 pop 해줘야함 ) 
+        while (delete <k){
+            delete++;
             st.pop();
-            k--;
-         }
-        
-        while(!st.isEmpty()) answer+=st.pop();
-        StringBuilder sb = new StringBuilder(answer);
+        }
         
         
-        return  sb.reverse().toString();
+        while (!st.isEmpty()){
+            answer+=st.pop();
+        }
+        
+        StringBuilder sb= new StringBuilder(answer);
+        answer = sb.reverse().toString();
+        
+        return answer;
     }
 }
